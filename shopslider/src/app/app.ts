2b7fdf81e2280fb81d43b4275ed0c88c5bcb62c2
James-stephenekele <james-stephenekele@gmail.com>
2025-09-23 00:10:44 +0100

    updted live commit

--- shopslider/src/app/app.ts
+++ shopslider/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { container } from "./containter/container";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ container],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ shopslider/src/app/containter/container.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-container',
+  
+  templateUrl: './container.html',
+  styleUrl: './container.css'
+})
+export class container {
+}
